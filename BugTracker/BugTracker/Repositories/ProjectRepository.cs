dc5250b69fc5e426f02fb244574978ea6965ceba
Antony Solis <antonysolis@live.ca>
2020-06-20 14:36:12 -0500

    Add functions to project repository

--- BugTracker/BugTracker/Repositories/ProjectRepository.cs
+++ BugTracker/BugTracker/Repositories/ProjectRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BugTracker.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,46 @@ namespace BugTracker.Repositories
 {
     public class ProjectRepository
     {
+        public ApplicationDbContext db = new ApplicationDbContext();
+        public ICollection<Project> GetAllProjects()
+        {
+            var result = db.Projects.ToList();
+
+            return result;
+        }
+
+        public ICollection<string> ProjectsForUser(int userId)
+        {
+            var result = db.Projects.Where(up => up.Id == userId).Select(p => p.Name);
+            return result.ToList();
+        }
+
+        public void AddProject(int projectId)
+        {
+            var project = db.Projects.FirstOrDefault(p => p.Id == projectId);
+
+            if (project.Id != projectId)
+            {
+                db.Projects.Add(project);
+
+                db.SaveChanges();
+            }
+            else
+            {
+                throw new Exception("Project Already Exists");
+            }
+        }
+
+        public void DeleteProject(Project projectId)
+        {
+            if (!db.Projects.Contains(projectId))
+            {
+                db.Projects.Remove(projectId);
+            }
+            else
+            {
+                throw new Exception("Project Does Not Exist");
+            }
+        }
     }
 }
